58d755b1ba6bf27a3ec81eab51eea0cc278f62c6
Ezequiel Prats <eze.prats13@gmail.com>
2025-03-25 10:53:48 -0300

    eaa

--- src/context/Context.js
+++ src/context/Context.js
@@ -2,32 +2,30 @@ import { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
 
 const NewsContext = createContext();
-
 export const useNewsContext = () => useContext(NewsContext);
 
 export const NewsProvider = ({ children }) => {
-    const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-    const BASE_URL = `https://gnews.io/api/v4/search?q=example&apikey=${API_KEY}`;
+    const API_KEY = process.env.NEXT_PUBLIC_NEWS_API_KEY;
+    const BASE_URL = "https://gnews.io/api/v4/search";
 
     const [articles, setArticles] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const[currentPage,setCurrentPage]=useState(1)
-    const[totalResults,setTotalResults]=useState(0);
-    const[category,setCategory]=useState("")
-    const [query,setQuery]=useState("")
-    console.log(API_KEY);
-    console.log(BASE_URL);
-    
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalResults, setTotalResults] = useState(0);
+    const [category, setCategory] = useState("");
+    const [query, setQuery] = useState("");
+
+    console.log("API_KEY:", API_KEY);
 
     const fetchNews = async () => {
         setLoading(true);
         setError(null);
-    
+
         try {
-            const response = await axios.get(`${BASE_URL}`, {
+            const response = await axios.get(BASE_URL, {
                 params: {
-                    apiKey: API_KEY, // ✅ Solo una vez
+                    apiKey: API_KEY,
                     country: "us",
                     page: currentPage,
                     pageSize: 6,
@@ -39,7 +37,7 @@ export const NewsProvider = ({ children }) => {
                     "Accept": "application/json",
                 }
             });
-    
+
             console.log("API Response:", response.data.articles);
             setArticles(response.data.articles);
             setTotalResults(response.data.totalResults);
@@ -50,20 +48,18 @@ export const NewsProvider = ({ children }) => {
             setLoading(false);
         }
     };
-    
-    
-    
+
     useEffect(() => {
         console.log("Fetching news...");
         fetchNews();
-    }, [currentPage,category,query]);
+    }, [currentPage, category, query]);
 
-    const changeCategory=(newCategory)=>{
-        setCategory(newCategory)
-    }
+    const changeCategory = (newCategory) => {
+        setCategory(newCategory);
+    };
 
     return (
-        <NewsContext.Provider value={{ articles, loading, error, fetchNews, currentPage,setCurrentPage, totalResults,category,changeCategory,setQuery }}>
+        <NewsContext.Provider value={{ articles, loading, error, fetchNews, currentPage, setCurrentPage, totalResults, category, changeCategory, setQuery }}>
             {children}
         </NewsContext.Provider>
     );
