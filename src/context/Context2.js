3b4e57071c97f683d30500f363164f7664836b7d
Ezequiel Prats <eze.prats13@gmail.com>
2025-04-09 19:13:08 -0300

    ea

--- src/context/Context2.js
+++ src/context/Context2.js
@@ -6,8 +6,8 @@ const NewsContext = createContext();
 export const useNewsContext = () => useContext(NewsContext);
 
 export const NewsProvider = ({ children }) => {
-    const API_KEY = process.env.REACT_APP_API_KEY;
-    const BASE_URL = "https://gnews.io/api/v4";
+    const API_KEY = "768c27130cc540d79423ad9e557beb96";
+    const BASE_URL = "https://newsapi.org/v2";
 
     const [articles, setArticles] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -20,32 +20,30 @@ export const NewsProvider = ({ children }) => {
     const fetchNews = async () => {
         setLoading(true);
         setError(null);
-      
+
         try {
-          const params = {
-            apikey: API_KEY,
-            country: "us",
-            page: currentPage,
-            pageSize: 13,
-            category: category || "general",
-          };
-      
-          
-          console.log("URL de solicitud:", `${BASE_URL}/top-headlines?${new URLSearchParams(params).toString()}`);
-      
-          const response = await axios.get(`${BASE_URL}/top-headlines`, { params });
-      
-          console.log("API Response:", response.data.articles);
-          setArticles(response.data.articles);
-          setTotalResults(response.data.totalResults);
+            const response = await axios.get(`${BASE_URL}/top-headlines`, {
+                params: {
+                    apiKey: API_KEY,
+                    country: "us",
+                    page:currentPage,
+                    pageSize:13,
+                    category,
+                    q:query,
+                }
+            });
+
+            console.log("API Response:", response.data.articles);
+            setArticles(response.data.articles);
+            setTotalResults(response.data.totalResults)
         } catch (err) {
-          console.error("Error fetching news:", err);
-          setError("Could not fetch data");
+            console.error("Error fetching news:", err);
+            setError("Could not fetch data");
         } finally {
-          setLoading(false);
+            setLoading(false);
         }
-      };
-      
+    };
+
     useEffect(() => {
         console.log("Fetching news...");
         fetchNews();
